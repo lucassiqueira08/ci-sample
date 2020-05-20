17de540e9feb2ef57d875cb9fb08fdffa8450ae1
Lucas Siqueira <lalvessiqueira8@gmail.com>
2020-05-20 20:23:56 -0300

    teste

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 app.get('/', (req, res) => {
-  res.send("Olá, classe!");
+  res.send("Fala, galera! Como vocês estão?");
 })
 
 app.listen(port);
